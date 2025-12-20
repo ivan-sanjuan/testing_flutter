ce4089260b04ac0e1eb6d41dc494cd855e85ed04
vashmeow <isanjuanivan@gmail.com>
2025-12-20 22:38:46 +0800

    i do not own this code -- for learning purposes only

--- lib/screens/BankingLogin.dart
+++ lib/screens/BankingLogin.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
+import ''
 
 class BankingLogin extends StatefulWidget {
-  const BankingLogin({super.key});
+  BankingLogin({super.key});
 
   @override
-  State<BankingLogin> createState() => _BankingLoginState();
+  State<BankingLogin> createState() => _BankingLoginState()
 }
 
-class _BankingLoginState extends State<BankingLogin> {
-
-  @override
-  Widget build(BuildContext context)
+class _BankingLoginState extends BankingLogin{
   
 }
